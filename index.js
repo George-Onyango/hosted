ed3b8b633ec9565c3cc24ecf47e085bd74f6201b
Junior <onyangogeorge954@gmail.com>
2022-08-12 18:55:19 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+const h2 = document.createElement("h2");
+h2.textContent = "Let's get it";
+
+// Add the h2 element to the body element in the DOM
+document.querySelector('body').appendChild(h2);
+
+// Create a paragraph element
+const p = document.createElement('p');
+p.textContent = 'In for a penny in for a pound'
+
+// Add the p element to the body element in the DOM
+document.querySelector('body').appendChild(p);
